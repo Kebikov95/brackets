3acf14b13e7c24f6ce2f63deb1e8bd9c4b7bb064
Kebikau Kiryl <kebikov1995@gmail.com>
2020-03-19 10:07:58 +0300

    feat: add functionality to solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  let brackets = [];
+  for(let i = 0; i < bracketsConfig.length; i++) {
+     brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < brackets.length;) {
+    if (str.indexOf(brackets[i]) !== -1) {
+      str = str.replace(brackets[i], '');
+      i = 0;
+    }
+    else i++;
+  }
+
+  return str.length == 0 ? true : false;
 }
